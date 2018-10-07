05873f6d3ee48caeb0fb953afaf8b6efa9060c70
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-07 16:19:41 +0530

    Initial commit

---
+++ binance_btc_balance.2m.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env /usr/local/bin/node
+require('./binance_btc_balance')();
---
+++ binance_btc_balance/index.js
@@ -0,0 +1,47 @@
+const Binance = require('binance-api-node').default;
+const { apiKey, apiSecret } = require('./secrets.json');
+
+function calculateBTCBalance(balances, prices) {
+  let totalBTCBalance = 0.0;
+
+  balances.forEach((balance) => {
+    let assetBtcValue;
+
+    const assetTotalBalance = parseFloat(balance.free) + parseFloat(balance.locked);
+
+    if (balance.asset === 'BTC') {
+      assetBtcValue = assetTotalBalance;
+    } else {
+      const assetPriceInBTC = prices[`${balance.asset}BTC`] ? prices[`${balance.asset}BTC`] : '0.0';
+      assetBtcValue = assetTotalBalance * parseFloat(assetPriceInBTC);
+    }
+
+    totalBTCBalance += assetBtcValue;
+  });
+
+  return totalBTCBalance;
+}
+
+async function getBTCBalance() {
+  const client = Binance({ apiKey, apiSecret });
+  let accountInfo; let
+    prices;
+
+  try {
+    accountInfo = await client.accountInfo({ recvWindow: 1000 });
+    prices = await client.prices({ recvWindow: 1000 });
+  } catch (e) {
+    console.log('...');
+    console.log(e.toString());
+    return;
+  }
+
+  const btcBalance = calculateBTCBalance(accountInfo.balances, prices);
+
+  console.log(
+    `BTC: ${btcBalance.toFixed(5)
+    } ($${(btcBalance * prices.BTCUSDT).toFixed(2)})`,
+  );
+}
+
+module.exports = getBTCBalance;
